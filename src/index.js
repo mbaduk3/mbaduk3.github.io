e5129452a59accf2386dbec170b94a0164c125cb
Maxim Baduk <mb2474@cornell.edu>
2020-04-22 23:09:07 -0400

    clean

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('sigma')
-import './favicon/favicon.ico'
-import './favicon/favicon-32x32.png'
-import './favicon/favicon-16x16.png'
-import './favicon/apple-touch-icon.png'
-import './index.css';
-
-console.log("hello world");
