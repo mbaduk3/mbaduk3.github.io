6561ee47f71bb450ad082177d5633fb8ffab789b
Maxim Baduk <mb2474@cornell.edu>
2020-04-22 22:54:22 -0400

    added webpack, node, sigma.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('sigma')
+import './favicon/favicon.ico'
+import './favicon/favicon-32x32.png'
+import './favicon/favicon-16x16.png'
+import './favicon/apple-touch-icon.png'
+import './index.css';
+
+console.log("hello world");
