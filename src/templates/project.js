fe5174085b26f759e974888d19c9b52b26a4500a
Maxim Baduk <mb2474@cornell.edu>
2020-06-04 10:05:38 -0400

    portfolio, all work pages

--- src/templates/project.js
+++ src/templates/project.js
@@ -7,14 +7,29 @@ const Project = ({ data, }) => {
    const { site, markdownRemark } = data;
    const { siteMetadata } = site;
    const { frontmatter, html } = markdownRemark;
+   const techList = frontmatter.technologies.map((tech, i) => {
+       return <li key={tech + "_" + i} className="project-tech-item">{tech}</li>
+   });
    return (
        <Layout>
            <Helmet>
                <title>{frontmatter.title} | {siteMetadata.title}</title>
-               <meta name="description" content={frontmatter.desc} />j
+               <meta name="description" content={frontmatter.desc} />
            </Helmet>
-           <h1 className="portfolio-entry-title">{frontmatter.title}</h1>
-           <p className="portfolio-entry-sub">{frontmatter.timeline}</p>
+           <div className="project-meta">
+            <div className="project-section">
+                <h1 className="portfolio-entry-title">{frontmatter.title}</h1>
+                <p className="portfolio-entry-sub">{frontmatter.timeline}</p>
+                </div>
+                <div className="project-section">
+                    <div className="project-section-head"><h2>Technologies</h2></div>
+                    <ul className="project-tech-list">{techList}</ul>
+                </div>
+                <div className="project-section">
+                    <div className="project-section-head"><h2>Overview</h2></div>
+                    <p className="project-desc">{frontmatter.desc}</p>
+                </div>
+            </div>
            <div className="project-content" dangerouslySetInnerHTML={{__html: html}}></div>
         </Layout>
    );
@@ -33,8 +48,10 @@ export const pageQuery = graphql`
             html
             frontmatter {
                 timeline
+                desc
                 slug
                 title
+                technologies
             }
         }
     }
